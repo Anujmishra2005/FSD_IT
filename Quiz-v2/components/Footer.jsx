d90b2f5bae0d8e61f4fa3aa60a16ba09b20944de
Anujmishra2005 <anujmishraan2005@gmail.com>
2024-11-21 13:04:12 +0530

    New Commit

---
+++ Quiz-v2/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+      Designed by FSD Team
+    </div>
+  )
+}
+
+export default Footer;
---
+++ Quiz-v2/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div id='header'>
+      ABES ENGINEERING COLLEGE GHAZIABAD
+    </div>
+  )
+}
+
+export default Header;
