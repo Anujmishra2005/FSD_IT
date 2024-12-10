9c566ebdb3809a954d304aae0c616de1fb48f1b8
Anujmishra2005 <anujmishraan2005@gmail.com>
2024-12-10 12:11:41 +0530

    New Commit

--- stopwatch-app/src/Components/Refex.jsx
+++ stopwatch-app/src/Components/Refex.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useRef } from 'react';
 const Refex = () => {
+    console.log("Object Rendered")
+    const refcount = useRef(0);
+    function handleIncrement(){
+        refcount.current++
+        if(refcount.current==5)
+            alert("current refcount="+refcount.current)
+        console.log("refcount=",refcount.current)
+    }
   return (
     <div>
-        <h1> useRef Example</h1>
+        <h1>UseRef Example</h1>
+        <h2>RefCount:{refcount.current}</h2>
+        <button onClick={handleIncrement}>Increment</button>
     </div>
   )
 }
